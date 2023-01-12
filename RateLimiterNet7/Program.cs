5c1d8cfca7efbc72e24d2e0f10a9acce71e59edf
KANNAPPANS <Kannappan.Somasundaram@thetrainline.com>
2023-01-12 09:32:04 +0000

    Rate Limiter middleware

---
+++ RateLimiterNet7/Program.cs
@@ -0,0 +1,56 @@
+using System.Net;
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+const string RateLimitingPolicyName = "user";
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter(RateLimitingPolicyName, opt =>
+    {
+        opt.Window = TimeSpan.FromSeconds(10);
+        opt.PermitLimit = 1;
+        opt.QueueLimit = 0;
+    });
+    
+    options.OnRejected = (context, _) =>
+    {
+        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.TooManyRequests;
+        if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+        {
+            context.HttpContext.Response.Headers.Add(HeaderNames.RetryAfter, retryAfter.ToString());
+        }
+        return new ValueTask();
+    };
+});
+
+var app = builder.Build();
+
+app.UseRateLimiter();
+
+app.MapGet("/user", context =>
+    {
+        Console.WriteLine($"timestamp : {DateTime.Now:MM/dd/yyyy hh:mm:ss.fff tt}");
+        return CreateResponse(context, "Hello User");
+    })
+    .RequireRateLimiting(RateLimitingPolicyName);
+
+app.MapGet("/admin", context => CreateResponse(context, "Hello Admin"));
+
+app.Run();
+
+Task CreateResponse(HttpContext httpContext, string message)
+{
+    return httpContext.Response.WriteAsJsonAsync(new Response
+    {
+        Message = message
+    });
+}
+
+public record Response
+{
+    public DateTime CreatedAt { get; } = DateTime.Now;
+    public string Message { get; init; }
+}
